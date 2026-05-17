69a5a03a0bd2e5b6f3c31a9fda44760ee224b2ee
abdullahasad45 <abdullah.asad@vaival.tech>
2026-05-17 19:40:04 +0500

    updated code form dev-1

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 //this is from main branch
 console.log("Hello World from main branch");
 //this is from again from main  branch
-console.log("Hello again from main branch");
+//this is from again from Dev-1  branch
+console.log("Hello World from Dev-1 branch");
 //this is from dev branch
 console.log("Hello World from dev branch");
