7ca15e8dae327a6470be7e35c26d932fa57a0071
abdullahasad45 <abdullah.asad@vaival.tech>
2026-05-17 19:33:25 +0500

    dev-code

--- app.js
+++ app.js
@@ -2,3 +2,5 @@
 console.log("Hello World from main branch");
 //this is from again from main  branch
 console.log("Hello again from main branch");
+//this is from dev branch
+console.log("Hello World from dev branch");
