ed0b7150249f71327df819f8b323ba936b158a39
abdullahdevopsengineer-afk <abdullah.devopsengineer@gmail.com>
2026-05-17 19:38:27 +0500

    Correct typo and add log from dev-2
    Fix typo in console log statement and add new log.

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 //this is from main branch
 console.log("Hello World from main branch");
 //this is from again from main  branch
-console.log("Hello again from main branch");
+//this form dev-2 on same branch 
+conssole.log("hello form dev-2")
 //this is from dev branch
 console.log("Hello World from dev branch");
